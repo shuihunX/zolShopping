ccb42f19628546e39716d2218295654f76db33d4
xpj <er6769649@gmail.com>
2020-11-12 22:01:24 +0800

    增删改查

--- dist/js/index.js
+++ dist/js/index.js
@@ -31,12 +31,28 @@ $(function() {
 
     /* 导航 + 轮播图 + 会员登录 */
     /* 导航列表 */
-    /*  $("main-nav_list").children().children().mouseenter(function() {
-         $(this).css("color", "red").siblings().css("color", "#474747");
-     }).mouseleave(function() {
-         $(this).css("color", "#474747");
-     }) */
+    $(".main-nav_list").children().children().mouseenter(function() {
+        $(this).css("background", "red").siblings().css("background", "#fff");
 
+        $(".main_nav_box").appendTo($('.lunbotu'));
+        $('.swiper-container').css("display", "none");
+        $(".main_nav_box").css({
+            "display": "block",
+        })
+    }).mouseleave(function() {
+        $(this).css("background", "#fff");
+        $(".main_nav_box").css("display", "none");
+        $('.swiper-container').css("display", "block");
+
+    })
+
+    /*  $("main_nav_box").mouseenter(function() {
+         $(".main_nav_box").css({
+             "display": "block",
+         })
+     }).mouseleave(function() {
+         $(".main_nav_box").css("display", "none");
+     }) */
 
 
 })
